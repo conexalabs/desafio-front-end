d8a83308d712b1e0bd6d4f6c1fa63d94051946ef
Anuar <anuar_daher@hotmail.com>
2019-12-13 00:29:44 -0300

    projeto

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'fa'
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: '#3a8970'
+      }
+    }
+  }
+})
