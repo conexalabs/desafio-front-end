2b8d52b442839fb403209147785fe73d001cbf61
Anuar <anuar_daher@hotmail.com>
2019-12-12 23:34:22 -0300

    buddle

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  'transpileDependencies': [
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/localizar-empresa/'
+    : '/',
+  transpileDependencies: [
     'vuetify'
   ]
 }
