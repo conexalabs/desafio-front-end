c8564a4c5a57e9a570a6da0bbb15079b8d88697b
rique223 <henrique.jobs1@gmail.com>
2020-07-15 20:39:48 -0300

    Projeto commitado

---
+++ src/App.ts
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <NavBar />
+      < /div>
+    )
+  }
+}
+export default App
