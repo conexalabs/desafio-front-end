8f7ffcde26beef16b22d98a58f033a9eac115397
Pedro Caetano <pedroccaetano@live.com>
2020-12-18 00:51:29 -0300

    Init localization page

--- src/pages/Localization/index.js
+++ src/pages/Localization/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
-function Localization() {
-  return <h1>localization</h1>;
+const stores = [
+  { latitude: -26.9853947, longitude: -52.603549, local: "Cordilheira Alta" },
+  { latitude: -26.9605363, longitude: -52.5335505, local: "Xaxim" },
+];
+
+export class Localization extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={13}
+        initialCenter={{ lat: -27.0922364, lng: -52.6166878 }}
+      >
+        {stores.map((store, index) => {
+          return (
+            <Marker
+              key={index}
+              id={index}
+              position={{
+                lat: store.latitude,
+                lng: store.longitude,
+              }}
+            />
+          );
+        })}
+      </Map>
+    );
+  }
 }
 
-export default Localization;
+export default GoogleApiWrapper((props) => ({
+  apiKey: "",
+}))(Localization);
