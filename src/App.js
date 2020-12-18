defbe29e518fb43d4e438ce96e1029dea81de024
Pedro Caetano <pedroccaetano@live.com>
2020-12-18 00:48:59 -0300

    Create company hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./styles/global.scss";
 
+import { CompanyProvider } from "./hooks/company";
 import Routes from "./routes";
 
 const App = () => (
-  <>
+  <CompanyProvider>
     <Routes />
-  </>
+  </CompanyProvider>
 );
 
 export default App;
