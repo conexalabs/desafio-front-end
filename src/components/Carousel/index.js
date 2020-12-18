fdd9055d2591b0ae53d6bf7370d9bfa7db48efc7
Pedro Caetano <pedroccaetano@live.com>
2020-12-18 20:36:45 -0300

    Create breakpoints carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,15 +6,29 @@ import Card from "../Card";
 import "./style.scss";
 
 function CarouselComponent({ cards }) {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1, itemsToScroll: cards.length },
+    {
+      width: 550,
+      itemsToShow: 2,
+      itemsToScroll: cards.length,
+    },
+    { width: 850, itemsToShow: 3 },
+    { width: 1150, itemsToShow: 4 },
+    { width: 1450, itemsToShow: 5 },
+    { width: 1750, itemsToShow: 6 },
+  ];
+
   return (
     <Carousel
+      breakPoints={breakPoints}
       enableSwipe={true}
       disableArrowsOnEnd={false}
       itemPosition={consts.CENTER}
     >
-      {cards.map((card) => (
+      {cards.map((card, index) => (
         <Card
-          key={card.cnpj}
+          key={index}
           companyName={card.companyName}
           cnpj={card.cnpj}
           address={card.address}
