8cfd6006f1467673eb23f93737a36e076fbd59e4
HerissonSilva <herissonsilvahs@gmail.com>
2021-02-02 05:06:59 -0300

    chore: integrations with google maps

--- src/views/MapPage.vue
+++ src/views/MapPage.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="map-page">
+  <div class="map-page" v-if="business">
     <BusinessCard
+      :companyName="business.nome"
+      :cnpj="business.cnpj"
+      :address="`
+        ${business.logradouro} ${business.numero}, ${business.bairro}, ${business.municipio} ${business.uf}
+      `"
       hasBackfowardButton
       class="float-card"
     />
@@ -8,43 +13,36 @@
       :width="$vuetify.breakpoint.width"
       :height="$vuetify.breakpoint.height"
       frameborder="0" style="border:0"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBBa-3HeIg11kR6qZMmcySAqnAtNfAn-R0
-        &q=Space+Needle,Seattle+WA" allowfullscreen>
+      :src="`https://www.google.com/maps/embed/v1/search?key=AIzaSyBBa-3HeIg11kR6qZMmcySAqnAtNfAn-R0
+        &q=${mapQuerySearch}`" allowfullscreen>
     </iframe>
   </div>
 </template>
 
 <script>
 import { mask } from 'vue-the-mask'
-import { mapActions, mapState } from 'vuex'
 import BusinessCard from '@/components/cards/BusinessCard.vue'
 
 export default {
   name: 'Home',
   data: () => ({
-    mapQuerySearch: ''
+    mapQuerySearch: '',
+    business: null
   }),
   directives: { mask },
   components: { BusinessCard },
-  computed: {
-    ...mapState({
-      businessList: state => state.businessList
-    })
-  },
   mounted () {
     if (!this.$route.params.business) this.$router.push({ name: 'Home' })
+
     const {
       logradouro,
-      numero,
       municipio,
+      bairro,
       uf
     } = this.$route.params.business
-    this.mapQuerySearch = `${logradouro} ${numero}, ${municipio} ${uf}`
-  },
-  methods: {
-    ...mapActions([
-      'addBusinessToListAction'
-    ])
+
+    this.mapQuerySearch = `${logradouro} ${bairro} ${municipio} ${uf}`.replace(/\s/g, '+')
+    this.business = this.$route.params.business
   }
 }
 </script>
@@ -53,6 +51,7 @@ export default {
 .map-page
   height: 100%
   .float-card
+    width: 18rem
     position: fixed
     left: 0
     top: 0
