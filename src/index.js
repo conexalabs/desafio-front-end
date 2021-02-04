ea785fd7cc88e2aa300c9df1ad5d2d3ea0740383
Marcos Henrique <makikin18@hotmail.com>
2021-02-04 20:42:07 -0300

    Pagina atualizada

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Routes from './src/Routes';
+import Routes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
