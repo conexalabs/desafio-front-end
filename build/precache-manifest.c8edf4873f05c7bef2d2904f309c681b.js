569b5c9a22b21beb487fe1141f63c27da40bac30
Marcos Henrique <makikin18@hotmail.com>
2021-02-04 23:41:16 -0300

    Build final

---
+++ build/precache-manifest.c8edf4873f05c7bef2d2904f309c681b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2c82061715aca3c6dc1f1b0c40e88e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd4b235d3ca5f72b63e2",
+    "url": "/static/css/main.f1a1ad1e.chunk.css"
+  },
+  {
+    "revision": "d26d87d0334d1995b7a1",
+    "url": "/static/js/2.423d1499.chunk.js"
+  },
+  {
+    "revision": "6b200a42712f0c7aba9e9a64a186c879",
+    "url": "/static/js/2.423d1499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4b235d3ca5f72b63e2",
+    "url": "/static/js/main.2d14d5ef.chunk.js"
+  },
+  {
+    "revision": "e5fa8845b52652435117",
+    "url": "/static/js/runtime-main.a4590638.js"
+  },
+  {
+    "revision": "22d28c6699905910e48450542929b639",
+    "url": "/static/media/arrow.22d28c66.svg"
+  },
+  {
+    "revision": "2c321ea6aa97ab17e78dadc544391101",
+    "url": "/static/media/back-button.2c321ea6.svg"
+  },
+  {
+    "revision": "3f22ada7eaa2fae27adbc80c1a0c32b2",
+    "url": "/static/media/loading.3f22ada7.svg"
+  },
+  {
+    "revision": "6b8c41bebce0a4384dc9857b30c78c50",
+    "url": "/static/media/predio.6b8c41be.svg"
+  },
+  {
+    "revision": "c0cecb47d2b7ad649c4e4ff59bfbef60",
+    "url": "/static/media/search-image.efb1c3da.png"
+  }
+]);
